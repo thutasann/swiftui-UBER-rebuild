be6b5837bc8cf857f748ca1b4b80eb4404869a50
thutasann <thutasann@gmail.com>
2022-12-06 14:57:27 +0630

    Initial Commit

---
+++ swiftui-uber-rebuild/swiftui_uber_rebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_uber_rebuildApp.swift
+//  swiftui-uber-rebuild
+//
+//  Created by Thuta sann on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_uber_rebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
